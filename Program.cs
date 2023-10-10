d10b6a0cfb35d9bd51a3e3be98283c6bd4030172
1AndOne1 <112853915+1AndOne1@users.noreply.github.com>
2023-10-10 23:51:13 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq.Expressions;
+
+class Calculator
+{
+    public int Sum(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Divide(int a, int b)
+    {
+
+            if (b != 0)
+            {
+                return a / b;
+            }
+
+            else
+            {
+                throw new DivideByZeroException("Cannot divide by zero!");
+            }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+
+        Console.Write("Enter the first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        int sum = calculator.Sum(a, b);
+        int diff = calculator.Subtract(a, b);
+        int mult = calculator.Multiply(a, b);
+        int quot = calculator.Divide(a, b);
+
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Difference: {diff}");
+        Console.WriteLine($"Product: {mult}");
+        Console.WriteLine($"Quotient: {quot}");
+    }
+}
